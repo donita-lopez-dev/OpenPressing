9414dab60c9745ee0493266b890259115912b104
donita lopez Guetuissie Mambou <118095818+donita-lopez-dev@users.noreply.github.com>
2022-11-19 12:31:18 +0100

    Creation Constraint_Attribut.sql

--- Constraint/Constraint_Attribut.sql
+++ Constraint/Constraint_Attribut.sql
@@ -1,1 +1,1 @@
-ALTER TABLE Attributs ADD CONSTRAINT FKAttributs356857 FOREIGN KEY (Types_servicesId_Pressing) REFERENCES Types_services (Id_Type);
+ALTER TABLE Attributs ADD CONSTRAINT FKAttributs356857 FOREIGN KEY (Types_servicesId_Pressing)REFERENCES Types_services (Id_Type);
