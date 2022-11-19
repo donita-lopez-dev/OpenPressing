dc2b11803da9fe7aabae850b71883cb65e62a9e3
donita lopez Guetuissie Mambou <118095818+donita-lopez-dev@users.noreply.github.com>
2022-11-19 15:10:07 +0100

    Create main.sql

---
+++ main.sql
@@ -0,0 +1,39 @@
+PROMPT******************** SET NLS LANGUAGE*********************
+ALTER SESSION SET NLS_LANGUAGE=French;
+
+
+PROMPT******************** SET NLS TERRITORY*********************
+ALTER SESSION SET NLS_TERRITORY=Cameroon;
+
+PROMPT******************** SET line size*********************
+set linesize 1000
+
+PROMPT******************** SET NLS DATE FORMAT*********************
+ALTER session set NLS_DATE_FORMAT='DD/MM/YYYY';
+
+PROMPT
+PROMPT
+
+PROMPT ************************************************************************************
+PROMPT ***********************************user creation and configuration******************
+PROMPT ************************************************************************************
+@users/main
+
+PROMPT
+PROMPT
+
+PROMPT ************************************************************************************
+PROMPT ***********************************creation of tables*******************************
+PROMPT ************************************************************************************
+@objects/main
+
+
+PROMPT
+PROMPT
+ 
+
+@data/main
+--@features/main
+SPOOL OFF
+
+SELECT table_name FROM user_tables; 
