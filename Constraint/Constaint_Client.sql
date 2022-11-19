4daf5eeb73204119707f9b08f6b9a17c52d2c7b4
donita lopez Guetuissie Mambou <118095818+donita-lopez-dev@users.noreply.github.com>
2022-11-19 08:08:22 +0100

    Create Constraint_Client.sql

--- Constraint/Constaint_Client.sql
+++ Constraint/Constaint_Client.sql
@@ -1,1 +1,1 @@
-ALTER TABLE Client ADD CONSTRAINT FKClient853690 FOREIGN KEY (UtilisateursId_utilisateur) REFERENCES Utilisateurs (Id_utilisateur);
+ALTER TABLE Client ADD CONSTRAINT FKClient853690 FOREIGN KEY (UtilisateursId_utilisateur)REFERENCES Utilisateurs (Id_utilisateur);
