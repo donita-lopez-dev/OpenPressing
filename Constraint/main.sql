fa37bd8d23e19810df09392cf39fd5a6071e91f8
donita lopez Guetuissie Mambou <118095818+donita-lopez-dev@users.noreply.github.com>
2022-11-19 16:54:40 +0100

    Create main.sql

---
+++ Constraint/main.sql
@@ -0,0 +1,57 @@
+PROMPT******************** add constrains_Adresse*************************
+@Constraint/constraint_Adresse
+
+PROMPT******************** add constrains_Annonces*************************
+@Constraint/constraint_Annonces
+
+PROMPT******************** add constrains_Attributs*************************
+@Constraint/constraint_Attribut
+
+PROMPT******************** add constrains_Besoins_Client*************************
+@Constraint/constraint_Besoins_Client 
+
+PROMPT******************** add constrains_Chat*************************
+@Constraint/constraint_Chat
+
+PROMPT******************** add constrains_Client*************************
+@Constraint/constraint_Client
+
+PROMPT******************** add constrains_Offre*************************
+@Constraint/constraint_Offre
+
+PROMPT******************** add constrains_Personne_physique*************************
+@Constraint/constraint_Personne_Physique 
+
+PROMPT******************** add constrains_Personnes*************************
+@Constraint/constraint_Personnes
+
+PROMPT******************** add constrains_Pressing*************************
+@Constraint/constraint_Pressing
+
+PROMPT******************** add constrains_Privilèges *************************
+@Constraint/constraint_Privilèges
+
+
+PROMPT******************** add constrains_Prix*************************
+@Constraint/constraint_Prix
+
+
+PROMPT******************** add constrains_Promo*************************
+@Constraint/constraint_Promo
+
+
+PROMPT******************** add constrains_Rôle*************************
+@Constraint/constraint_Rôle
+
+
+PROMPT******************** add constrains_Services*************************
+@Constraint/constraint_Services
+
+
+PROMPT******************** add constrains_Valeur_Attributs*************************
+@Constraint/constraint_Valeur_Attributs
+
+
+PROMPT******************** add constrains_Linges_Client*************************
+@Constraint/constraint_Linges_Client
+
